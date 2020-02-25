3a51a74d0d0d882de9ab8be12f7e092af595c5ed
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-24 22:25:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// const multiply = () => {
+//   const num1 = 31;
+//   const num2 = 2;
+//   return num1*num2;
+// }
+
+const num1 = 31;
+const num2 = 2;
+const array = [1, 2, 3, 20]
+
+const multiply = num1*num2;
+
+// const random = () => {
+//   const random = Math.random(1,30)
+// }
+const random = Math.random(1,30);
+
+const mod = 20%8;
+
+const max = Math.max(1,2,3,20)
